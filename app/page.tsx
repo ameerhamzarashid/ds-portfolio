fe342f02951725d6feeb266fd302c3a960a774a4
ameerhamzarashid <ameerhamzauk9@gmail.com>
2026-05-07 14:04:34 +0100

    Add gamified data tickets station doors and inventory

--- app/page.tsx
+++ app/page.tsx
@@ -15,14 +15,22 @@ import ThemeBackground from "@/components/ThemeBackground";
 import JourneyScene3D from "@/components/JourneyScene3D";
 import JourneyOverlay from "@/components/JourneyOverlay";
 import JourneyGamification from "@/components/JourneyGamification";
+import FloatingDataTickets from "@/components/FloatingDataTickets";
+import ProjectStationDoors from "@/components/ProjectStationDoors";
+import InventoryDrawer from "@/components/InventoryDrawer";
+import RouteMiniMap from "@/components/RouteMiniMap";
 
 export default function Home() {
   return (
     <main>
       <ThemeBackground />
       <JourneyScene3D />
+      <RouteMiniMap />
       <JourneyOverlay />
       <JourneyGamification />
+      <FloatingDataTickets />
+      <InventoryDrawer />
+
       <Navbar />
       <IllusionHero />
       <TechMarquee />
@@ -31,6 +39,7 @@ export default function Home() {
       <Skills />
       <TechCommandCentre />
       <DataVisuals />
+      <ProjectStationDoors />
       <Projects />
       <CaseStudies />
       <Experience />
